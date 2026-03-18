f62b28360e03be89a90c53d321b908fba68026d4
ee17btech11034 <asiwalraja@gmail.com>
2026-03-18 11:34:07 +0530

    Virtual DOm, Reconciliation, Fiber, hydration

--- Intro.js
+++ Intro.js
@@ -1,69 +1,40 @@
 /*
+    # Virtual Dom:
+        --> Light weight tree
+        --> React does not use V-DOM directly, internal functanality has been changed
+        --> React.createRoot 
+                --> create a DOM and compare with older one
+                --> this DOm is virtual 
+        --> Browser DOM:
+                --> Browser also creates DOM
+                --> Repaint the pixels according to changes
+                --> It is Page Reloading or rendering
+        
+    # Fiber:
+        --> Read https://github.com/acdlite/react-fiber-architecture
+        --> Suppose we are tracking some network request:
+            --> which will have 3- 4 states. Like will stablish connection, receive headers, receive body, confirmation message, etc.
+            --> Earlier in react, React used to reflect changes instantly. 
+            --> Now here we can see i will update this UI component frequently whereas 
+                --> If I can wait then I will wait for sometime and then I will show the current state. which will save frequent re-renders. 
+            --> Think of this in a complex frontend where multiple nextwork calls are updating UI.
 
-Hooks:
-    --> Create a counter with variable as let and state.
-
-Counter with variable:
-    --> JS is updating counter but DOM is not reacting to it.
-    --> react does this job but its own way
-
-React need:
-    --> Suppose I want to show this counter value in 
-        --> "counter para" :=>  <p> Counter: {counter}</p> 
-        --> increment :=>  <button onClick={incrementCounter}>Inc {counter}</button>
-        --> decrementfunc :=>  <button onClick={decrementCounter}>Dec {counter}</button>
-        --.somewhere in footer: => somewhere using `{counter}`
-    --> I will have to take the reference using queryselector and then run these methods on all of these to update the DOM.
-
---> React controls the UI and JS control the variables. 
-    --> Hooks provide the functionality to React or JS to be in sync to rerender that particular component
-    --> Data update only through hooks to track by react
-    --> Once component rerenders then JS variable "reassigned the initial val" whereas state keep their state
-
---> useState Hook:
-    --> built-in React Hook that lets you add state to functional components.
-    --> const [state, setState] = useState(initialValue);
-        --> state: Current state value.
-        --> setState: Function to update the state.
-        --> initialValue: Initial value (can be any type — number, string, object, array, etc
-    --> Important:
-        --> Always destructure the array returned by useState. 
-        --> Never mutate state directly (e.g., count++).  Always use the setter (setCount).
-        --> State Updates Are Asynchronous
-            --> State updates do not reflect immediately. 
-            --> React batches updates for performance.
-    --> Rules:
-        --> Only call at the top level — not inside loops, conditions, or nested functions. 
-        --> Call in the same order on every render. 
-        --> Use custom Hooks to reuse stateful logic.
-    --> Best pratice:
-        --> Use multiple useState calls for unrelated values.
-            --> const [data, setData] = useState(() => heavyCalculation());     ==> valid
-        --> Use functional initialization for expensive computations:
-            --> useState(obj/array/val) but                useState(function) is not optimized 
-        --> Avoid unnecessary state — only store what affects rendering.
+        --> React people said we will use Fiber to handle these.
+        --> Virtual DOM is created and then Fiber handles things by comparing only the changed components
     
---> Mutate State:
-    --> In this App we used
-        --> let [counter, setCounter] = useState(0)
-            --> counter++
-            --> setCounter(counter)
-    --> Issues here:
-        --> 1. You're mutating state directly:
-                --> you're still mutating a value that should be treated as immutable.
-                --> because here UI got re-rendered because of I think "onclick" function on button. 
-                --> Or else there is no way react can find that state got updated.
-        --> 2. Breaks Pure Components & Optimizations:
-                --> If your component or child components use React.memo, useMemo, or PureComponent, 
-                        they rely on reference equality. Direct mutation can cause incorrect behavior or missed updates.
-        --> 3. Fragile and Confusing and not future proof:
-                --> This pattern makes code harder to reason about. 
-                --> Future developers (or you) might assume it's safe to mutate state elsewhere, leading to bugs
-    --> write these:
-        --> const [counter, setCounter] = useState(0) //making counter as immutable
-        --> setCounter(counter + 1); // Better
-                    // or
-            setCounter(counter => counter + 1); // Best (functional update)
+    # hydration:
+        --> HTMl, CSS is loaded and page layout is done
+        --> But JS file is still coming to browser.
+        --> This situation where JS file is not present yet --> called Hydration.
+        --> in this, buttons will be there but we can not use them
+    
+    # Reconciliation:
+        --> algo which diff the V-DOM.
+        --> react decides which one will re-render.
+        --> Cases:
+            --> different component type will be totally updated.
+            --> diff of list is performed by "keys"
 
---> No taking reference of DOm elements and then update. Just make this state and let React handle everything
-*/      
+
+            --> If fiber is going to make change to DOm and in that time state is changed then it will drop that DOm work and do the necessary steps to show new data.
+*/
